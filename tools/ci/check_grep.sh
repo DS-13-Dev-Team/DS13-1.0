cddb9d47c4a3fabef9f7489cc8723f9c9f8e2d17
DeadLineSS13 <41364830+DeadLineSS13@users.noreply.github.com>
2021-01-06 16:35:40 +0300

    Update check_grep.sh

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -8,46 +8,46 @@ shopt -s globstar
 st=0
 
 echo "Checking for TGM formatting"
-if grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
+if grep -El '^\".+\" = \(.+\)' maps/DeadSpace/*.dmm;	then
     echo "Non-TGM formatted map detected. Please convert it using Map Merger!"
     st=1
 fi;
 echo "Checking for mapping tags"
-if grep -nP '^\ttag = \"icon' _maps/**/*.dmm;	then
+if grep -nP '^\ttag = \"icon' maps/DeadSpace/*.dmm;	then
     echo "tag vars from icon state generation detected in maps, please remove them."
     st=1
 fi;
 echo "Checking for step_[xy]"
-if grep -nP 'step_[xy]' _maps/**/*.dmm;	then
+if grep -nP 'step_[xy]' maps/DeadSpace/*.dmm;	then
     echo "step_x/step_y variables detected in maps, please remove them."
     st=1
 fi;
 echo "Checking for stacked cables"
-if grep -nP '"\w+" = \(\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/area/.+\)' _maps/**/*.dmm;	then
+if grep -nP '"\w+" = \(\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/area/.+\)' maps/DeadSpace/*.dmm;	then
     echo "found multiple cables on the same tile, please remove them."
     st=1
 fi;
 echo "Checking for cable varedits"
-if grep -nP '/obj/structure/cable(/\w+)+\{' _maps/**/*.dmm;	then
+if grep -nP '/obj/structure/cable(/\w+)+\{' maps/DeadSpace/*.dmm;	then
     echo "ERROR: vareditted cables detected, please remove them."
     st=1
 fi;
 echo "Checking for cable d1/d2"
-if grep -nP '\td[1-2] =' _maps/**/*.dmm;	then
+if grep -nP '\td[1-2] =' maps/DeadSpace/*.dmm;	then
     echo "ERROR: d1/d2 cable variables detected in maps, please remove them."
     st=1
 fi;
 echo "Checking for pixel_[xy]"
-if grep -nP 'pixel_[xy] = 0' _maps/**/*.dmm;	then
+if grep -nP 'pixel_[xy] = 0' maps/DeadSpace/*.dmm;	then
     echo "pixel_x/pixel_y = 0 variables detected in maps, please review to ensure they are not dirty varedits."
 fi;
 echo "Checking for vareditted areas"
-if grep -nP '^/area/.+[\{]' _maps/**/*.dmm;	then
+if grep -nP '^/area/.+[\{]' maps/DeadSpace/*.dmm;	then
     echo "Vareditted /area path use detected in maps, please replace with proper paths."
     st=1
 fi;
 echo "Checking for base /turf paths"
-if grep -nP '\W\/turf\s*[,\){]' _maps/**/*.dmm; then
+if grep -nP '\W\/turf\s*[,\){]' maps/DeadSpace/*.dmm; then
     echo "base /turf path use detected in maps, please replace with proper paths."
     st=1
 fi;
@@ -114,22 +114,24 @@ if grep -ni 'nanotransen' code/**/*.dm; then
     echo "Misspelling(s) of nanotrasen detected in code, please remove the extra N(s)."
     st=1
 fi;
-if grep -ni 'nanotransen' _maps/**/*.dmm; then
+if grep -ni 'nanotransen' maps/**/*.dmm; then
     echo "Misspelling(s) of nanotrasen detected in maps, please remove the extra N(s)."
     st=1
 fi;
-if ls _maps/*.json | grep -nP "[A-Z]"; then
-    echo "Uppercase in a map json detected, these must be all lowercase."
-	st=1
-fi;
-for json in _maps/*.json
-do
-    filename="_maps/$(jq -r '.map_path' $json)/$(jq -r '.map_file' $json)"
-    if [ ! -f $filename ]
-    then
-        echo "found invalid file reference to $filename in _maps/$json"
-        st=1
-    fi
-done
+
+#IN FUTURE Create Map change in BUILD.
+#if ls maps/*.json | grep -nP "[A-Z]"; then
+#    echo "Uppercase in a map json detected, these must be all lowercase."
+#	st=1
+#fi;
+#for json in maps/*.json
+#do
+#    filename="maps/$(jq -r '.map_path' $json)/$(jq -r '.map_file' $json)"
+#    if [ ! -f $filename ]
+#    then
+#        echo "found invalid file reference to $filename in _maps/$json"
+#        st=1
+#    fi
+#done */
 
 exit $st
