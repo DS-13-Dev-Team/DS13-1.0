23928a8832488e3e96430b558bc53decbfb480f8
DeadLine <mishah8888@mail.ru>
2021-11-18 18:53:09 +0300

    Test

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 tools/deploy.sh ci_test
 mkdir ci_test/config
 
-#test config
-cp tools/ci/ci_config.txt ci_test/config/config.txt
-
 cd ci_test
 DreamDaemon baystation12.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
