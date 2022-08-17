aa10bf74f82f5abb4d5fca611c04b28dc8a2a211
DTraitor <sehhd32@gmail.com>
2022-08-18 00:59:05 +0300

    grid

--- tgui/packages/tgui/components/Grid.js
+++ tgui/packages/tgui/components/Grid.js
@@ -1,38 +1,52 @@
-/**
- * @file
- * @copyright 2020 Aleksej Komarov
- * @license MIT
- */
-
-import { Table } from './Table';
 import { pureComponentHooks } from 'common/react';
+import { Box } from './Box';
 
-/** @deprecated */
-export const Grid = (props) => {
-  const { children, ...rest } = props;
+export const Grid = props => {
+  const {
+    columns,
+    rows,
+    gridSize,
+    children,
+    ...rest
+  } = props;
+  const columnEmpty = [];
+  const rowEmpty = [];
+
+  // Some spaghetti code to make -ms-grid work properly
+  for (let i = 1; i <= columns+1; i++) {
+    columnEmpty.push(<Box width={gridSize} height={gridSize} style={{ "-ms-grid-column": i, "-ms-grid-row": 1 }} />);
+  }
+
+  for (let i = 2; i <= rows+1; i++) {
+    rowEmpty.push(<Box width={gridSize} height={gridSize} style={{ "-ms-grid-column": 1, "-ms-grid-row": i }} />);
+  }
+
   return (
-    <Table {...rest}>
-      <Table.Row>{children}</Table.Row>
-    </Table>
+    <Box style={{ "display": "-ms-grid" }} {...rest}>
+      {columnEmpty}
+      {rowEmpty}
+      {children}
+    </Box>
+  );
+};
+
+export const GridItem = props => {
+  const {
+    firstColumn,
+    firstRow,
+    secondColumn,
+    secondRow,
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <Box style={{ "-ms-grid-column": firstColumn, "-ms-grid-row": firstRow, "-ms-grid-column-span": secondColumn, "-ms-grid-row-span": secondRow }} {...rest}>
+      {children}
+    </Box>
   );
 };
 
 Grid.defaultHooks = pureComponentHooks;
-
-/** @deprecated */
-export const GridColumn = (props) => {
-  const { size = 1, style, ...rest } = props;
-  return (
-    <Table.Cell
-      style={{
-        width: size + '%',
-        ...style,
-      }}
-      {...rest}
-    />
-  );
-};
-
-Grid.defaultHooks = pureComponentHooks;
-
-Grid.Column = GridColumn;
+GridItem.defaultHooks = pureComponentHooks;
+Grid.Item = GridItem;
