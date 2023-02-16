03589ae4fda18661a835bedbff9c738c9dda8c35
DTraitor <sehhd32@gmail.com>
2023-02-16 17:22:08 +0200

    another test

--- tools/bootstrap/node.bat
+++ tools/bootstrap/node.bat
@@ -1,4 +1,5 @@
 @echo off
+set NODE_OPTIONS=--openssl-legacy-provider
 set NODE_SKIP_PLATFORM_CHECK=1
 call powershell -NoLogo -ExecutionPolicy Bypass -File "%~dp0\node_.ps1" Download-Node
 for /f "tokens=* USEBACKQ" %%s in (`
